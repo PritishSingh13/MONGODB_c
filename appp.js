f5dbdc1924c2c2e74c8ef9e60e99ab3ed50a3f10
PritishSingh13-Git <pritishsg13@gmail.com>
2024-04-23 10:03:25 +0530

    Initial Commit

---
+++ appp.js
@@ -0,0 +1,80 @@
+const express =require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3002;
+
+app.use(express.json());//middleware...parsing the data from object to json format.
+
+
+//connect to MongoDB
+
+mongoose.connect('mongodb://127.0.0.1:27017/user_management_db')
+.then(()=> console.log('Connected to MongoDB'))
+.catch(err=> console.error('error connecting to mongoDB: ',err));
+
+
+
+//Define user schema
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('User',userSchema);
+
+//routeHandlers
+
+//GET
+app.get('/users',(req,res)=>{
+    User.find({})
+    .then(users => res.json(users))
+    .catch(err => res.status(500).json({message: err.message}));
+});
+
+//POST
+app.post('/users',(req,res)=>{
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save()
+    .then(newUser => res.status(201).json(newUser))
+    .catch(err => res.status(400).json({message:err.message}));
+});
+
+//PUT
+app.put('/users/:id',(req,res)=>{
+    const userId = req.params.id;
+    const updateData={
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    User.findByIdAndUpdate(userId, updateData, {new: true})
+    .then(updatedUser =>{
+        if(!updatedUser){
+            return res.status(404).json({message: 'User not found'});
+        }
+        res.json(updatedUser);
+    })
+    .catch(err=> res.status(400).json({message: err.message}));
+});
+
+app.listen(PORT);
+
+app.delete('users/:id',(req , res)=>{
+const userId = req.params.id;
+
+User.findByIdAndDelete(userId)
+.then(deleteUser=>{
+    if(deleteUser{
+        return res.status(404).json({message:deleteUser);
+    })
+})
+});
